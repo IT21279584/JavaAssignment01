8a166622bb3e9a83205f0ab126113161c1c6a473
HansakaJS <hansakajayawarna@gmail.com>
2023-12-15 12:09:39 +0530

    Update

--- src/main/java/Exercise03/Main.java
+++ src/main/java/Exercise03/Main.java
@@ -1,9 +1,7 @@
 package Exercise03;
 
-import java.util.logging.Logger;
-
 public class Main {
-    static Logger logger = Logger.getLogger(Logger.GLOBAL_LOGGER_NAME);
+
     public static void main(String[] args){
 
         Car car1 = new Car("Toyota", "Vitz", "White", 2013, 1400000,5, 5,false);
@@ -15,16 +13,16 @@ public class Main {
 
         Inventory inventory = new Inventory(vehicleList);
 
-        logger.info("Average price of vehicles : "+inventory.getAveragePrice());
+        System.out.println("Average price of vehicles : "+inventory.getAveragePrice());
 
         String searchMake = "Toyota";
         String searchModel = "Vitz";
         Vehicle[] searchVehicles = inventory.searchByMakeAndModel(searchMake, searchModel);
 
-        logger.info("Vehicle make with "+searchMake+" and model with "+searchModel);
+        System.out.println("Vehicle make with "+searchMake+" and model with "+searchModel);
 
         for(Vehicle searchVehicle : searchVehicles){
-            logger.info("The Result is match make with "+searchVehicle.getMake()+" and model with "+ searchVehicle.getModel());
+            System.out.println("The Result is match make with "+searchVehicle.getMake()+" and model with "+ searchVehicle.getModel());
         }
     }
 }
